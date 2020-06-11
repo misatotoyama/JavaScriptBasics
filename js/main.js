40cfc1e836fa0112fba09d920c9485b1444c5912
misatotoyama <misato030306@gmail.com>
2020-06-11 15:59:34 +0900

    switchの練習

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ let centi = 500;
 
 centi += 100;
 centi *= 10;
-centi -=1;
+centi -= 1;
 
 console.log(centi);
 
 
 
-const price= 1200;
+const price = 1200;
 
 console.log(price > 1000);
 console.log(500 < 1000);
@@ -24,14 +24,41 @@ console.log(price !== 1000);
 console.log(Boolean(0));
 console.log(Boolean('hello'));
 
-const score = 90;
+const gram = 90;
 
-if (score >= 80){
+if (gram >= 80) {
     console.log('Great!');
-    }else if (score >= 60){
-        console.log('Good.')
-    }else{
-        console.log('ok...');
-    }
+} else if (gram >= 60) {
+    console.log('Good.')
+} else {
+    console.log('ok...');
+}
 
-score >= 80 ? console.log('great!') : console.log('OK~');
+gram >= 80 ? console.log('great!') : console.log('OK~');
+
+
+const score = 60;
+const name = 'taguchi';
+
+if (score >= 50) {
+    if (name === 'taguchi') {
+        console.log('Good job!');
+    }
+}
+
+const signal = 'pink';
+
+switch (signal) {
+    case 'red':
+        console.log('stop!');
+        break;
+    case 'yellow':
+        console.log('Caution!!');
+        break;
+    case 'blue':
+    case 'green':
+        console.log('Go!');
+        break;
+        default:
+            console.log('wrongsignal');
+}
